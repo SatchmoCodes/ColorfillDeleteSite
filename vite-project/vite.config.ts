57ecc4e16843da25316c8e8e2df94b5ad0514034
ColeZap <cjshuman12@gmail.com>
2025-11-13 21:30:44 -0500

    hopefully this works

--- vite-project/vite.config.ts
+++ vite-project/vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/https://satchmocodes.github.io/ColorfillDeleteSite/",
+  base: "/ColorfillDeleteSite/",
   plugins: [react()],
 });
