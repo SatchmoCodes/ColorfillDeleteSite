02cfc933217c51150dd8c734c3bdadbc919bf071
ColeZap <cjshuman12@gmail.com>
2025-11-13 21:26:31 -0500

    update again

--- vite-project/vite.config.ts
+++ vite-project/vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "/https://satchmocodes.github.io/ColorfillDeleteSite/",
   plugins: [react()],
 });
